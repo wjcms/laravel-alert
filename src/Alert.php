c7ac768f28433f246d96eed7a09851dad6cd0f83
13149563301 <1059114241@qq.com>
2020-09-30 19:47:11 +0800

    修复bug

--- src/Alert.php
+++ src/Alert.php
@@ -32,15 +32,11 @@ class Alert
         ]);
         session()->flash('alerts', $this->alerts);
 
-
         if($callback == 'back'){
-            sleep(3);
             return back();
         }else if(preg_match('/^((https|http)?:\/\/)[^\s]+/', $callback)){
-            sleep(3);
             return redirect()->away($callback);
         }else{
-            sleep(3);
             return redirect()->route($callback);
         }
     }
